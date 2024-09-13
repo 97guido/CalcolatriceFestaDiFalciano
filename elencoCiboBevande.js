b2d5ed5cb0d0886c3c0f17f886725d4537b3c1e5
97guido <97guido@gmail.com>
2024-09-13 18:33:28 +0200

    First commit

---
+++ elencoCiboBevande.js
@@ -0,0 +1,158 @@
+var items = [
+    {
+        "name": "Birra alla spina PICCOLA [€3,50]",
+        "prezzo": 3.5,
+        "quantita": 0,
+        "id_num": "num_birra_pic"
+    },
+    {
+        "name": "Birra alla spina MEDIA [€5]",
+        "prezzo": 5,
+        "quantita": 0,
+        "id_num": "num_birra_med"
+    },
+    {
+        "name": "Bibita lattina [€2,50]",
+        "prezzo": 2.5,
+        "quantita": 0,
+        "id_num": "num_lattina"
+    },
+    {
+        "name": "Birra bottiglia [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_birra_bott"
+    },
+    {
+        "name": "Bottiglia vino 0,75L [€5]",
+        "prezzo": 5,
+        "quantita": 0,
+        "id_num": "num_bott_vino075"
+    },
+    {
+        "name": "Bottiglia vino 1/4L [€2]",
+        "prezzo": 2,
+        "quantita": 0,
+        "id_num": "num_bott_vino14"
+    },
+    {
+        "name": "Bottiglia acqua GRANDE [€2]",
+        "prezzo": 2,
+        "quantita": 0,
+        "id_num": "num_acqua_grand"
+    },
+    {
+        "name": "Bottiglia acqua PICCOLA [€1]",
+        "prezzo": 1,
+        "quantita": 0,
+        "id_num": "num_acqua_pic"
+    },
+    {
+        "name": "Caffè [€1,20]",
+        "prezzo": 1.2,
+        "quantita": 0,
+        "id_num": "num_caffe"
+    },
+    {
+        "name": "Caffè corretto [€1,50]",
+        "prezzo": 1.5,
+        "quantita": 0,
+        "id_num": "num_caffe_corr"
+    },
+    {
+        "name": "Liquore [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_liquore"
+    },
+
+
+
+
+
+
+    {
+        "name": "Patatine [€3]",
+        "prezzo": 3,
+        "quantita": 0,
+        "id_num": "num_pata"
+    },
+    {
+        "name": "Hamburger [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_hamb"
+    },
+    {
+        "name": "Solo piada [€1]",
+        "prezzo": 1,
+        "quantita": 0,
+        "id_num": "num_piada"
+    },
+    {
+        "name": "Piada con prosciutto [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_pros"
+    },
+    {
+        "name": "Piada crudo, casatella e rucola [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_piada_cru_ca_ru"
+    },
+    {
+        "name": "Piada con salame [€6]",
+        "prezzo": 6,
+        "quantita": 0,
+        "id_num": "num_piada_salame"
+    },
+    {
+        "name": "Piada con salsiccia [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_salsic"
+    },
+    {
+        "name": "Piada casatella e rucola [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_casa_ruc"
+    },
+    {
+        "name": "Piada con Gratin (4 pz.) [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_piada_grat"
+    },
+    {
+        "name": "Strozzapreti [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_strozzap"
+    },
+    {
+        "name": "Trippa [€7,50]",
+        "prezzo": 7.5,
+        "quantita": 0,
+        "id_num": "num_trippa"
+    },
+    {
+        "name": "Cotiche con fagioli [€7]",
+        "prezzo": 7,
+        "quantita": 0,
+        "id_num": "num_cotiche_fagio"
+    },
+    {
+        "name": "Gratin (4 pz.) [€6]",
+        "prezzo": 6,
+        "quantita": 0,
+        "id_num": "num_gratin"
+    },
+    {
+        "name": "Insalata [€2,50]",
+        "prezzo": 2.5,
+        "quantita": 0,
+        "id_num": "num_insalata"
+    },
+]
---
+++ main.js
@@ -0,0 +1,99 @@
+var tot = 0;
+
+
+function setButtonNames() {
+    document.querySelectorAll('div > div > div > input[type=button]').forEach(el => {
+        if (el.id != '') {
+            var idToFind = el.id.replaceAll('btn_','num_');
+            items.forEach(item => {
+                if (idToFind == item.id_num) el.value = item.name;
+            });
+        }
+    });
+}
+
+function addToItem(idItem) {
+    
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    ++(items[numItem].quantita);
+    document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+    updateConto();
+}
+
+// function addToItem(numItem) {
+//     ++(items[numItem].quantita);
+//     document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+//     updateConto();
+// }
+
+// function removeToItem(numItem) {
+//     if (items[numItem].quantita < 1) return;
+//     --(items[numItem].quantita);
+//     document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+//     updateConto();
+// }
+
+function removeToItem(idItem) {
+
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    if (items[numItem].quantita < 1) return;
+    --(items[numItem].quantita);
+    document.getElementById(items[numItem].id_num).value = items[numItem].quantita;
+    updateConto();
+}
+
+// function updateNumber(numItem) {
+//     var qty = Number(document.getElementById(items[numItem].id_num).value);
+//     if (qty < 0) qty = 0;
+//     items[numItem].quantita = qty;
+//     updateConto();
+// }
+
+function updateNumber(idItem) {
+
+    var numItem = -1;
+    
+    items.forEach((el, index) => {
+        if (el.id_num == idItem) numItem = index;
+    });
+
+    if (numItem == -1) return;
+
+    var qty = Number(document.getElementById(items[numItem].id_num).value);
+    if (qty < 0) qty = 0;
+    items[numItem].quantita = qty;
+    updateConto();
+}
+
+function updateConto() {
+    tot = 0;
+    
+    items.forEach(el => {
+        tot += el.prezzo * el.quantita;
+        // document.getElementById(el.id_num).value = el.quantita;
+    });
+    document.getElementById('totBox').innerHTML = `€ ${tot}`.replaceAll('.', ',');
+}
+
+function cancellaTutto() {
+    tot = 0;
+    items.forEach(el => {
+        el.quantita = 0;
+    });
+    document.getElementById('totBox').innerHTML = 'Totale:';
+    updateConto();
+}
